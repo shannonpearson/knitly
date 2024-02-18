cce749b6462a4456981224a4b6404f39bac50504
shannonpearson <schristinep@gmail.com>
2024-02-18 23:57:36 +0100

    connect db

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,15 +1,35 @@
+import "dotenv/config";
 import express, { Express, Request, Response } from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-import dotenv from "dotenv";
-
-dotenv.config();
+import { Client } from "pg";
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(cors());
+
+const DB_USER = process.env.DB_USER;
+const DB_PW = process.env.DB_PASSWORD;
+const DB_HOST = process.env.DB_HOST;
+const DB_PORT = process.env.DB_PORT;
+const DB_NAME = process.env.DB_NAME;
+const connectionString = `postgres://${DB_USER}:${DB_PW}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+const client = new Client(connectionString);
+
+(async () => {
+	await client.connect();
+
+	client
+		.query("SELECT * FROM users limit 1;")
+		.then((data) => {
+			console.log("DATA:", data.rows);
+		})
+		.catch((error: string) => {
+			console.log("ERROR:", error);
+		});
+})();
 
 app.get("/", (req: Request, res: Response) => {
 	res.send("Express + TypeScript Server");
--- db/init_db.sql
+++ db/init_db.sql
@@ -2,6 +2,11 @@ CREATE TABLE IF NOT EXISTS users (
   id uuid PRIMARY KEY,
   firstname text,
   lastname text,
+  username text,
+  password text,
   created_on timestamp,
-  update_on timestamp
+  updated_on timestamp
 );
+
+INSERT INTO users (id, firstname, lastname, username, password, created_on, updated_on)
+VALUES ('1ae3ebd5-177f-4fda-a5b2-f21b7e4bba64', 'Jean-Luc', 'Picard', 'captain', 'random', NOW(), NOW());
