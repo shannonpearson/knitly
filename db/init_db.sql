3804371c1782f1a4e394fce1442024496c8dbddd
shannonpearson <schristinep@gmail.com>
2024-02-19 23:40:53 +0100

    update schema to use email

--- db/init_db.sql
+++ db/init_db.sql
@@ -2,11 +2,12 @@ CREATE TABLE IF NOT EXISTS users (
   id uuid PRIMARY KEY,
   firstname text,
   lastname text,
-  username text,
+  email text,
   password text,
   created_on timestamp,
-  updated_on timestamp
+  updated_on timestamp,
+  last_login timestamp
 );
 
-INSERT INTO users (id, firstname, lastname, username, password, created_on, updated_on)
-VALUES ('1ae3ebd5-177f-4fda-a5b2-f21b7e4bba64', 'Jean-Luc', 'Picard', 'captain', 'random', NOW(), NOW());
+INSERT INTO users (id, firstname, lastname, email, password, created_on, updated_on)
+VALUES ('1ae3ebd5-177f-4fda-a5b2-f21b7e4bba64', 'Jean-Luc', 'Picard', 'jlp@starfleet.org', 'random', NOW(), NOW());
