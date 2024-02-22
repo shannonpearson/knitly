085b2ac6d28b5c41681e4b448e738f850f4a47a1
shannonpearson <schristinep@gmail.com>
2024-02-22 18:00:59 +0100

    add override for signup callback

--- api/src/auth.ts
+++ api/src/auth.ts
@@ -1,7 +1,23 @@
 import supertokens from "supertokens-node";
+import type { RecipeInterface } from "supertokens-node/recipe/emailpassword";
 import Session from "supertokens-node/recipe/session";
 import EmailPassword from "supertokens-node/recipe/emailpassword";
 
+const signUpOverride = (
+	originalImplementation: RecipeInterface
+): RecipeInterface => {
+	return {
+		...originalImplementation,
+		signUp: async (input) => {
+			let response = await originalImplementation.signUp(input);
+			// Post sign up response, we check if it was successful
+			if (response.status === "OK" && response.user.loginMethods.length === 1) {
+				const { id, emails } = response.user;
+			}
+			return response;
+		},
+	};
+};
 const init = () =>
 	supertokens.init({
 		framework: "express",
@@ -18,7 +34,9 @@ const init = () =>
 			websiteBasePath: "/auth",
 		},
 		recipeList: [
-			EmailPassword.init(), // initializes signin / sign up features
+			EmailPassword.init({
+				override: { functions: signUpOverride },
+			}), // initializes signin / sign up features
 			Session.init(), // initializes session features
 		],
 	});
