809b087c12da18f1a34a3addacc3ef12108693aa
shannonpearson <schristinep@gmail.com>
2024-02-22 18:00:09 +0100

    WIP add prisma

--- db/init_db.sql
+++ db/init_db.sql
@@ -3,11 +3,10 @@ CREATE TABLE IF NOT EXISTS users (
   firstname text,
   lastname text,
   email text,
-  password text,
   created_on timestamp,
   updated_on timestamp,
   last_login timestamp
 );
 
-INSERT INTO users (id, firstname, lastname, email, password, created_on, updated_on)
-VALUES ('1ae3ebd5-177f-4fda-a5b2-f21b7e4bba64', 'Jean-Luc', 'Picard', 'jlp@starfleet.org', 'random', NOW(), NOW());
+INSERT INTO users (id, firstname, lastname, email, created_on, updated_on)
+VALUES ('1ae3ebd5-177f-4fda-a5b2-f21b7e4bba64', 'Jean-Luc', 'Picard', 'jlp@starfleet.org', NOW(), NOW());
