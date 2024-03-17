3b87ed22f99c3125317e5fcefbcd9150fc8097ca
shannonpearson <schristinep@gmail.com>
2024-03-17 16:25:29 +0100

    init pinia

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,13 +1,16 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
 import App from './App.vue'
 import router from './router'
 import initAuth from './auth/initSuperTokens'
 
+const pinia = createPinia()
 const app = createApp(App)
-
 app.use(router)
+app.use(pinia)
 
 initAuth()
 
