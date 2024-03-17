6c061930111e336126f4b8fe8572f04b51e4fd28
shannonpearson <schristinep@gmail.com>
2024-03-17 18:39:56 +0100

    show user on profile

--- client/src/views/ProfileView.vue
+++ client/src/views/ProfileView.vue
@@ -1,12 +1,26 @@
 <template>
   <div>
     <h1>My Profile</h1>
+    <div>{{ user }}</div>
   </div>
 </template>
 
 <script lang="ts">
+import { useLoggedInUserStore } from '@/stores/loggedInUser'
+import type User from '@/types/User'
+
 export default {
-  name: 'ProfileView'
+  name: 'ProfileView',
+  data() {
+    return {
+      loggedInUserStore: useLoggedInUserStore(),
+      user: null as User | null
+    }
+  },
+  created() {
+    console.log('current user', this.loggedInUserStore.user)
+    this.user = this.loggedInUserStore.user
+  }
 }
 </script>
 
