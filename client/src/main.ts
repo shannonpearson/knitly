f134fe1236c74824e25b941b955cce4347268d9a
shannonpearson <schristinep@gmail.com>
2024-03-19 12:02:07 +0100

    add vuetify

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,6 +3,17 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives
+})
+
 import App from './App.vue'
 import router from './router'
 import initAuth from './auth/initSuperTokens'
@@ -10,6 +21,7 @@ import initAuth from './auth/initSuperTokens'
 const pinia = createPinia()
 const app = createApp(App)
 app.use(router)
+app.use(vuetify)
 app.use(pinia)
 
 initAuth()
